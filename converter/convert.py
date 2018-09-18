03c8d8acd86d7547e5e2e2653cca713381506d4c
tanzhijian <i@tanzhijian.org>
2018-09-18 17:16:45 +0800

    优化了转换脚本

--- converter/convert.py
+++ converter/convert.py
@@ -1,17 +1,16 @@
 # -*- coding: utf-8 -*-
-
 """
 把 csv 转换为可供 Dictionary Development Kit 处理的 xml 的脚本
 """
 
 import csv
-import os
+from pathlib import Path
 
-from jinja2 import Environment, FileSystemLoader
+from jinja2 import Template
 
-data = os.getcwd() + '/data.csv'
-template = 'converter/template.xml'
-output_file = 'converter/data.xml'
+data = Path(Path.cwd(), 'data.csv')
+template = Path(Path.cwd(), 'converter/template.xml')
+output_file = Path(Path.cwd(), 'converter/data.xml')
 
 
 def read_data(data):
@@ -22,24 +21,23 @@ def read_data(data):
         一个可供 jinja2 转换操作的双层嵌套列表，其中每个元素为 csv 数据集中的一行数据
     """
     with open(data) as f:
-        f_csv = csv.reader(f)
-        __ = next(f_csv)
-        entry_list = [row for row in f_csv]
-    return entry_list
+        entries = list(csv.reader(f))
+    return entries
 
 
-def render_template(entry_list, template):
+def render_template(entries, template):
     """用预先准备好的模板将读取的词典数据渲染为
         可供 Dictionary Development Kit 处理的数据格式
     Args: 
-        entry_list: 已读取的可供 jinja2 转换操作的双层嵌套列表，
+        entries: 已读取的可供 jinja2 转换操作的双层嵌套列表，
             其中每个元素为 csv 数据集中的一行数据
         template: 预先准备好可供渲染的 xml 模板文件，在当前文件夹中
     Returns: 
         一份渲染完毕的可供 Dictionary Development Kit 处理的文本字符串
     """
-    env = Environment(loader=FileSystemLoader('./'))
-    return env.get_template(template).render(entry_list=entry_list)
+    with open(template) as f:
+        xml = f.read()
+    return Template(xml).render(entries=entries)
 
 
 def main():
